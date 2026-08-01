7575d836da9f1c55b9acbdda279cec2489a7cbf7
shivduttsingh <shivduttsingh950@gmail.com>
2026-08-01 20:53:30 +0530

    Backtest every Nandi evidence component

--- nandi_oi/__init__.py
+++ nandi_oi/__init__.py
@@ -1,12 +1,14 @@
 """Nandi unified option-chain probability engine."""
 
 from .engine import NandiOIEngine
+from .evidence_backtest import EvidenceBacktester
 from .models import Decision, OptionLeg, OptionSnapshot
 from .upstox import UpstoxAPIError, UpstoxOptionChainClient
 from .unified_backtest import UnifiedBacktester
 
 __all__ = [
     "NandiOIEngine",
+    "EvidenceBacktester",
     "Decision",
     "OptionLeg",
     "OptionSnapshot",

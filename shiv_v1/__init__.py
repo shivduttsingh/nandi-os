f4f71adc17965f03610d892dfae6ad39d662b1e5
shivduttsingh <shivduttsingh950@gmail.com>
2026-08-22 21:25:54 +0530

    Add Shiv advanced research package

---
+++ shiv_v1/__init__.py
@@ -0,0 +1,5 @@
+"""Shiv-only experimental research engine.
+
+This package is intentionally isolated from Nandi's stable main branch.  It is
+paper/research logic only and contains no broker-order actions.
+"""
